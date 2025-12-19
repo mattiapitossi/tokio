cec26e3ada4ba0c777952712949d1b74724ee7fe
Mattia Pitossi <mattiapitossi@gmail.com>
2025-12-19 17:28:25 +0100

    add alt for benches

--- benches/ct_spawn.rs
+++ benches/ct_spawn.rs
@@ -1,6 +1,6 @@
 use gungraun::{library_benchmark, library_benchmark_group, main};
 use std::hint::black_box;
-use tokio::runtime::Runtime;
+use tokio::runtime::{self, Runtime};
 
 fn single_rt() -> tokio::runtime::Runtime {
     tokio::runtime::Builder::new_current_thread()
@@ -10,7 +10,15 @@ fn single_rt() -> tokio::runtime::Runtime {
 
 fn multi_rt() -> tokio::runtime::Runtime {
     tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(1)
+        .worker_threads(4)
+        .build()
+        .unwrap()
+}
+
+fn rt() -> Runtime {
+    runtime::Builder::new_multi_thread()
+        .worker_threads(NUM_WORKERS)
+        .enable_all()
         .build()
         .unwrap()
 }
@@ -39,6 +47,24 @@ fn spawn100(runtime: Runtime) {
         }
     });
 }
+const NUM_WORKERS: usize = 5;
+const NUM_SPAWN: usize = 10_000;
+
+fn rt_multi_spawn_many_remote_idle(rt: Runtime) {
+    let mut handles = Vec::with_capacity(NUM_SPAWN);
+
+    for _ in 0..100 {
+        for _ in 0..NUM_SPAWN {
+            handles.push(rt.spawn(async {}));
+        }
+
+        rt.block_on(async {
+            for handle in handles.drain(..) {
+                handle.await.unwrap();
+            }
+        });
+    }
+}
 
 #[library_benchmark]
 #[bench::basic(setup = single_rt)]
@@ -54,9 +80,15 @@ fn spawn_rt_100(runtime: Runtime) {
     black_box(spawn100(runtime));
 }
 
+#[library_benchmark]
+#[bench::threaded(setup = rt)]
+fn multi_spawn(runtime: Runtime) {
+    black_box(rt_multi_spawn_many_remote_idle(runtime));
+}
+
 library_benchmark_group!(
     name = group;
-    benchmarks = spawn_rt,spawn_rt_100
+    benchmarks = spawn_rt,spawn_rt_100,multi_spawn
 );
 
 main!(library_benchmark_groups = group);
